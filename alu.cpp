a7ecc6f50f284b922acb3c9ebd0c6c67ec10d664
sagarrakshe <sagar@sagar-liquid>
2012-09-15 09:17:02 +0530

    added alu.h for generating random no. and addressMap function

---
+++ alu.cpp
@@ -0,0 +1,12 @@
+#include "alu.h"
+#include <iostream>
+ALU::ALU() {
+}
+
+int ALU::genRand() {
+	return 0;
+}
+
+int ALU::addressMap() {
+	return 0;
+}
---
+++ alu.h
@@ -0,0 +1,13 @@
+#ifndef ALU_H_
+#define ALU_H_
+
+class ALU {
+
+	public:
+	
+	ALU();
+	int genRand();
+	int addressMap();
+};
+
+#endif /*ALU_H_*/
